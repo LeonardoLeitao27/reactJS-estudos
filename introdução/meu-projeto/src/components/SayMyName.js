2cadc93f076addd34ba015400c6ca42a9ea5fb32
Leonardo <leoleitao27@gmail.com>
2022-08-02 11:23:39 -0300

    css

--- introdução/meu-projeto/src/components/SayMyName.js
+++ introdução/meu-projeto/src/components/SayMyName.js
@@ -1,9 +1,12 @@
+import styles from './SayMyName.module.css'
+
 //usado na aula de props
 function SayMyName(props){
 
     return(
-        <div>
+        <div className={styles.frase}>
             <p>Fala ai {props.nome}</p>
+            <h1>Corno vei</h1>
         </div>
     ) 
 }
